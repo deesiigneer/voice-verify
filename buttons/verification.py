a9d270d9e12f61f9fcf4c3ba76cfebbbee1e8a3f
deesiigneer <xdeesiigneerx@gmail.com>
2022-08-19 21:45:14 +0300

    role fix

--- buttons/verification.py
+++ buttons/verification.py
@@ -1,6 +1,7 @@
 from nextcord import ButtonStyle, Interaction
 from nextcord.ui import View, Button, button
 from sys import exc_info
+from os import environ
 
 
 class verification(View):
@@ -10,12 +11,16 @@ class verification(View):
     @button(label="Подтвердить", row=0, style=ButtonStyle.green, custom_id=f'verification')
     async def verification_button(self, btn: Button, interaction: Interaction):
         try:
-            if interaction.response.is_done() is False:
-                await interaction.response.send_message(f"Ты уже прошел верефикацию...",
-                                                        ephemeral=True)
-            elif interaction.response.is_done() is True:
-                await interaction.followup.send(f"Ты уже прошел верефикацию...",
-                                                ephemeral=True)
+            role_id = int(environ.get('role_id', None))
+            if interaction.user.get_role(role_id) is None:
+                await interaction.user.add_roles(interaction.guild.get_role(role_id), reason='Верефикация')
+            else:
+                if interaction.response.is_done() is False:
+                    await interaction.response.send_message(f"Ты уже прошел верефикацию...",
+                                                            ephemeral=True)
+                elif interaction.response.is_done() is True:
+                    await interaction.followup.send(f"Ты уже прошел верефикацию...",
+                                                    ephemeral=True)
         except Exception as error:
             print(error, f'\nat line {exc_info()[2].tb_lineno}')
             if interaction.response.is_done() is False:
