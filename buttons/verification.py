bf8f3b078058218f2083458a19c6ca5b30bb2bc9
deesiigneer <xdeesiigneerx@gmail.com>
2022-08-19 21:48:11 +0300

    typo fix

--- buttons/verification.py
+++ buttons/verification.py
@@ -13,13 +13,13 @@ class verification(View):
         try:
             role_id = int(environ.get('role_id', None))
             if interaction.user.get_role(role_id) is None:
-                await interaction.user.add_roles(interaction.guild.get_role(role_id), reason='Верефикация')
+                await interaction.user.add_roles(interaction.guild.get_role(role_id), reason='Верификация')
             else:
                 if interaction.response.is_done() is False:
-                    await interaction.response.send_message(f"Ты уже прошел верефикацию...",
+                    await interaction.response.send_message(f"Ты уже прошел верификацию...",
                                                             ephemeral=True)
                 elif interaction.response.is_done() is True:
-                    await interaction.followup.send(f"Ты уже прошел верефикацию...",
+                    await interaction.followup.send(f"Ты уже прошел верификацию...",
                                                     ephemeral=True)
         except Exception as error:
             print(error, f'\nat line {exc_info()[2].tb_lineno}')
